14841af47e7ffbffe5a4f10a4415e0ecd8267632
LMS <abir2005qw@gmail.com>
2026-04-14 21:31:55 +0600

    Add files via upload

---
+++ LinkedHashSet Wrapper Class (Java).java
@@ -0,0 +1,51 @@
+package com.simplewrapper;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetWrapper<T> {
+
+    
+    private LinkedHashSet<T> set;
+
+    
+    public LinkedHashSetWrapper() {
+        set = new LinkedHashSet<>();
+    }
+
+
+    public boolean add(T element) {
+        return set.add(element);
+    }
+
+    
+    public boolean remove(T element) {
+        return set.remove(element);
+    }
+
+    
+    public boolean contains(T element) {
+        return set.contains(element);
+    }
+
+    
+    public int size() {
+        return set.size();
+    }
+
+    
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    
+    public void clear() {
+        set.clear();
+    }
+
+    
+    public void display() {
+        for (T item : set) {
+            System.out.println(item);
+        }
+    }
+}
